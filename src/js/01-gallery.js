1b85859ffda3756c1406e5a63c3994c04b9e6b94
volodymyr1v <volodumur.verhyn@gmail.com>
2023-05-27 14:22:08 +0300

    first commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
 
-console.log('hello  my world');
+const galleryEl = document.querySelector('.gallery');
+
+let createPicture = galleryItems.map(showImage).join('');
+
+function showImage ( {preview, original, description} ) {
+    return `
+    <li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+       </a>
+    </li>
+    `
+};  
+
+galleryEl.insertAdjacentHTML('afterbegin', createPicture);
+
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ }); 
